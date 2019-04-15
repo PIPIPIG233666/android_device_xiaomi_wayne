b6c37f527213867e857faf13d9770d209c97fa19
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    wayne: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- mk_wayne.mk
+++ mk_wayne.mk
@@ -23,6 +23,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/wayne.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from wayne device
 $(call inherit-product, device/xiaomi/wayne/device.mk)
 
